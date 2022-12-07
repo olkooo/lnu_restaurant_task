f0d355f53feba17d5e100f0b7ca9f57be7ea0219
bogdanpetrovsky <bpetrovsky977@gmail.com>
2022-12-07 22:07:09 +0200

    added configuration files and e2e setup

---
+++ frontend/canteen/src/app/secret.ts
@@ -0,0 +1,8 @@
+export const SECRET = {
+  MID: 'xxxxxxxxxxxxxxxxxxxx',
+  WEBSITE: 'xxxxxxxxxxxxxxxxxxxx',
+  INDUSTRY_TYPE_ID: 'xxxxxxxxxxxxxxxxxxxx',
+  CHANNEL_ID: 'xxxxxxxxxxxxxxxxxxxx',
+  CALLBACK_URL: 'xxxxxxxxxxxxxxxxxxxx',
+  PAYMENT_URL: 'xxxxxxxxxxxxxxxxxxxx',
+}
